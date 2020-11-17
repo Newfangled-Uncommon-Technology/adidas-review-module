7c8e39deb8515d0d60e727a77ff7556e9bc53b6e
dbumiller <d.m.bumiller@gmail.com>
2020-11-16 18:39:54 -0800

    Got webpack working

---
+++ client/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>it's all coming together now</div>
+    )
+  }
+}
