71ab8a5a5bc5207e084e1c80f88e16dab01fc5c0
dbumiller <d.m.bumiller@gmail.com>
2020-11-16 19:04:47 -0800

    Got one axios request getting data

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component{
   constructor() {
     super();
-    this.state = {
 
+    this.state = {
+      reviews: [],
+      reviewStats: [],
+      reviewCount: 0,
+      showCount: 2
     }
+
+    this.getReviewStats = this.getReviewStats.bind(this);
+  }
+
+  componentDidMount() {
+    this.getReviewStats();
+  }
+
+  getReviewStats() {
+    axios.get('/api/reviews/M20324')
+    .then((results) => {
+      this.setState({
+        reviews: results.data[0],
+        reviewCount: results.data[0].reviewCount
+      });
+      console.log(this.state.reviews);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
   }
 
   render() {
-    return (
-      <div>it's all coming together now</div>
-    )
+    if (this.state.reviews === []) {
+      return (
+        <div>Ratings and Reviews</div>
+      )
+    } else {
+      return (
+        <div>{this.state.reviewCount}</div>
+      )
+    }
   }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
-ReactDOM.render(<div>this is progress</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
